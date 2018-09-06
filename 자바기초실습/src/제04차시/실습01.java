4e872440f5e31aaa58b21f7176f4cc83910ca6f4
user <user@JBTC10118>
2018-09-06 15:09:22 +0900

    최우성

---
+++ 자바기초실습/src/제04차시/실습01.java
@@ -0,0 +1,16 @@
+package 제04차시;
+import java.io.IOException;
+public class 실습01 {
+
+	public static void main(String[] args) {
+		int bt;
+		try {
+			bt=System.in.read();
+			System.out.print((char)bt);
+		}catch(IOException e) {
+			System.err.println(e);
+		}
+
+	}
+
+}
